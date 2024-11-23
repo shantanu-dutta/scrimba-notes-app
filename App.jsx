3ee469057a0dce89b491bb92df6628151e020605
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2024-11-23 11:43:02 +0530

    feat(notes): implement note update, delete.

--- App.jsx
+++ App.jsx
@@ -2,77 +2,98 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Editor from "./components/Editor";
 import Split from "react-split";
-import { addDoc, onSnapshot } from "firebase/firestore";
-import { notesCollection } from "./firebase";
+import { addDoc, onSnapshot, deleteDoc, setDoc } from "firebase/firestore";
+import { getNoteRef, notesCollection } from "./firebase";
+
+const LoadingState = {
+  Loading: "Loading",
+  Loaded: "Loaded",
+  LoadError: "LoadError",
+};
 
 export default function App() {
+  const [loadingState, setLoadingState] = React.useState(LoadingState.Loading);
   const [notes, setNotes] = React.useState([]);
-  const [currentNoteId, setCurrentNoteId] = React.useState(notes[0]?.id || "");
+  const [currentNoteId, setCurrentNoteId] = React.useState("");
 
   const currentNote =
     notes.find((note) => note.id === currentNoteId) || notes[0];
 
   React.useEffect(() => {
     return onSnapshot(notesCollection, (snapshot) => {
-      console.log("latest notes: ", snapshot.docs);
+      console.log("notes updated.");
       const notesArr = snapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
       }));
+      console.log("latest notes:", notesArr);
       setNotes(notesArr);
+      setLoadingState(LoadingState.Loaded);
     });
   }, []);
+
+  React.useEffect(() => {
+    if (!currentNoteId) {
+      setCurrentNoteId(notes[0]?.id);
+    }
+  }, [notes]);
 
   async function createNewNote() {
     const newNote = {
       body: "# Type your markdown note's title here",
+      createAt: Date.now(),
+      updatedAt: Date.now(),
     };
     const newNoteRef = await addDoc(notesCollection, newNote);
     setCurrentNoteId(newNoteRef.id);
   }
 
-  function updateNote(text) {
-    setNotes((oldNotes) => {
-      const newArray = [];
-      for (let i = 0; i < oldNotes.length; i++) {
-        const oldNote = oldNotes[i];
-        if (oldNote.id === currentNoteId) {
-          // Put the most recently-modified note at the top
-          newArray.unshift({ ...oldNote, body: text });
-        } else {
-          newArray.push(oldNote);
-        }
-      }
-      return newArray;
-    });
+  async function updateNote(text) {
+    const noteRef = getNoteRef(currentNoteId);
+    await setDoc(
+      noteRef,
+      { body: text, updatedAt: Date.now() },
+      { merge: true }
+    );
   }
 
-  function deleteNote(event, noteId) {
+  async function deleteNote(event, noteId) {
     event.stopPropagation();
-    setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
+    const noteRef = getNoteRef(noteId);
+    await deleteDoc(noteRef);
   }
+
+  
 
   return (
     <main>
-      {notes.length > 0 ? (
-        <Split sizes={[30, 70]} direction="horizontal" className="split">
-          <Sidebar
-            notes={notes}
-            currentNote={currentNote}
-            setCurrentNoteId={setCurrentNoteId}
-            newNote={createNewNote}
-            deleteNote={deleteNote}
-          />
-          {currentNoteId && notes.length > 0 && (
+      {loadingState === LoadingState.Loading ? (
+        <div className="no-notes">
+          <h1>Loading notes...</h1>
+        </div>
+      ) : loadingState === LoadingState.Loaded ? (
+        notes.length > 0 ? (
+          <Split sizes={[30, 70]} direction="horizontal" className="split">
+            <Sidebar
+              notes={notes}
+              currentNote={currentNote}
+              setCurrentNoteId={setCurrentNoteId}
+              newNote={createNewNote}
+              deleteNote={deleteNote}
+            />
             <Editor currentNote={currentNote} updateNote={updateNote} />
-          )}
-        </Split>
+          </Split>
+        ) : (
+          <div className="no-notes">
+            <h1>You have no notes</h1>
+            <button className="first-note" onClick={createNewNote}>
+              Create one now
+            </button>
+          </div>
+        )
       ) : (
         <div className="no-notes">
-          <h1>You have no notes</h1>
-          <button className="first-note" onClick={createNewNote}>
-            Create one now
-          </button>
+          <h1>Error fetching notes.</h1>
         </div>
       )}
     </main>
--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore"
+import { getFirestore, collection, doc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -8,10 +8,14 @@ const firebaseConfig = {
   projectId: "react-notes-12dde",
   storageBucket: "react-notes-12dde.firebasestorage.app",
   messagingSenderId: "719966000918",
-  appId: "1:719966000918:web:12c2df4f38b1a70eeee237"
+  appId: "1:719966000918:web:12c2df4f38b1a70eeee237",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-export const notesCollection = collection(db, "notes")
+const db = getFirestore(app);
+const NOTES_COLLECTION_NAME = "notes";
+export const notesCollection = collection(db, NOTES_COLLECTION_NAME);
+export function getNoteRef(noteId) {
+  return doc(db, NOTES_COLLECTION_NAME, noteId);
+}
